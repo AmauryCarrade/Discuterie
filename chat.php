a0277a1dad0cb5d4e085a7c62df717bb712aaab6
J-C Bubbendorf <jc.bubbendorf@gmail.com>
2013-02-09 15:18:22 +0100

    Rooms: creation (protected, public or private), reaching. On database: new tables for allowances, bans (not used for the moment) and operators.

--- chat.php
+++ chat.php
@@ -3,7 +3,36 @@
 
 	$pdo = SQLiteConnect();
 
-	$rooms = $pdo->query('SELECT * FROM rooms WHERE type != \'private\'')->fetchAll();
+	$rooms = $pdo->query('SELECT rooms.id AS room_id, 
+								 rooms.name AS room_name, 
+								 rooms.type AS room_type,
+								 rooms.added AS room_added,
+								 rooms.creator AS creator_id
+						  FROM rooms
+						  WHERE 
+						  	rooms.type != \'private\'
+						  ORDER BY room_name');
+	$rooms = $rooms->fetchAll();
+
+	$allowedRoomsQ = $pdo->prepare('SELECT roomId FROM usersAllowed WHERE userId = ?');
+	$allowedRoomsQ->execute(array($_SESSION['user']['id']));
+	$allowedRoomsQ = $allowedRoomsQ->fetchAll();
+	$allowedRooms = array();
+	foreach ($allowedRoomsQ as $allowedRoom) {
+		$allowedRooms[] = $allowedRoom['roomId'];
+	}
+
+	$bannedRoomsQ = $pdo->prepare('SELECT roomId FROM usersBanned WHERE userId = ?');
+	$bannedRoomsQ->execute(array($_SESSION['user']['id']));
+	$bannedRoomsQ = $bannedRoomsQ->fetchAll();
+	$bannedRooms = array();
+	foreach ($bannedRoomsQ as $bannedRoom) {
+		$bannedRooms[] = $bannedRoom['roomId'];
+	}
+
+	$random = mt_rand(0, 123458) .  microtime();
+	$_SESSION['auth'] = sha1(SALT . sha1(SALT . $random));
+	$token = sha1(SALT . $random);
 ?>
 
 <!DOCTYPE html>
@@ -68,12 +97,14 @@
 		    <div class="row">
 			    <div class="span3">
 			    	<div class="well sidebar-nav">
-						<ul class="nav nav-list">
+						<ul class="nav nav-list" id="menuRoomsList">
 							<li class="nav-header">Salons</li>
-							<li class=""><a href="#"><em>Aucun salon</em></a></li>
+							<li id="listRoomsNoRooms"><a href="#"><em>Aucun salon</em></a></li>
+						</ul>
 							<hr />
-							<li><a href="#">Liste des salons</a></li>
-							<li><a href="#">Rejoindre un salon</a></li>
+						<ul class="nav nav-list">	
+							<li class="goToListRooms active"><a href="#">Liste des salons</a></li>
+							<li class="newRoom"><a href="#">Nouveau salon</a></li>
 						</ul>
 					</div><!--/.well -->
 				</div>
@@ -84,14 +115,14 @@
 		    				<h2>
 		    					<div class="pull-right">
 		    					    <div class="btn-group">
-										<button class="btn" title="Quitter le salon" disabled="disabled">
-											<span class="icon-remove"></span>
+										<button class="btn" disabled="disabled">
+											<?php echo $_SESSION['user']['name']; ?>
 										</button>
 										<ul class="dropdown-menu">
 											<li class="active"><a href="#">Connecté</a></li>
-											<li><a href="#">Absent</a></li>
-											<li><a href="#">Ne pas déranger</a></li>
-											<li><a href="#">Invisible</a></li>
+											<li class="disabled"><a href="#">Absent</a></li>
+											<li class="disabled"><a href="#">Ne pas déranger</a></li>
+											<li class="disabled"><a href="#">Invisible</a></li>
 											<li class="divider"></li>
 											<li><a href="?do=out">Déconnexion</a></li>
 										</ul>
@@ -101,7 +132,7 @@
 										</a>
 									</div>
 			    				</div>
-		    					Aucun salon
+		    					<span id="roomName">Tous les salons</span>
 		    				</h2>
 		    			</div>
 		    			<div id="content">
@@ -121,8 +152,8 @@
 			    	</div>
 			    </div>
 			    <div class="span3">
-			    	<div class="well sidebar-nav" style="display: none;">
-						<ul class="nav nav-list">
+			    	<div class="well sidebar-nav" style="display: none;" id="frameListConnectedUsers">
+						<ul class="nav nav-list" id="listConnectedUsers">
 							<li class="nav-header">Utilisateurs connectés</li>
 						</ul>
 					</div><!--/.well -->
@@ -134,7 +165,7 @@
 		<div style="display: none;">
 			
 			<div id="roomsList">
-				<div class="alert alert-info" id="noticeNotConnected">
+				<div class="alert alert-info noticeNotConnected">
 					Vous n'êtes connecté(e) à aucun salon.<br />
 					<small>
 						Choisissez ci-dessous un salon à joindre. 
@@ -143,22 +174,25 @@
 					</small>
 				</div>
 				<div class="pull-right">
-					<button class="newRoom btn btn-primary">Créer un salon</button>
+					<button class="btn btn-primary newRoom">Créer un salon</button>
 				</div>
 				
 				<div class="input-append">
-					<input type="text" id="secretRoom" class="input-xlarge" placeholder="Joindre un salon: son nom ?" />
-					<button class="btn btn-primary">Joindre</button>
+					<form class="formSecretRoom control-group">
+						<input type="text" class="secretRoom" class="input-xlarge" placeholder="Joindre un salon: son nom ?" autocomplete="off" />
+						<button class="btn btn-primary">Joindre</button>
+					</form>
 				</div>
 				
 				<h3>Liste des salons</h3>
-				    <table class="table table-striped">
+				    <table class="table table-striped" id="tableRooms">
 						<thead>
 							<tr>
 								<th>#</th>
 								<th>Nom du salon</th>
 								<th>Connectés</th>
 								<th>Joindre</th>
+								<th>&nbsp;</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -167,7 +201,7 @@
 									?>
 
 									<tr class="noRooms">
-										<td colspan="4" style="text-align: center; font-style: italic;">Aucun salon n'existe. Créez-en un à l'aide du bouton.</td>
+										<td colspan="5" style="text-align: center; font-style: italic;">Aucun salon n'existe. Créez-en un à l'aide du bouton.</td>
 									</tr>
 
 									<?php
@@ -177,10 +211,19 @@
 										?>
 
 										<tr>
-											<td><?php echo $room['id']; ?></td>
-											<td><?php echo $room['name']; ?></td>
-											<td></td>
-											<td><button class="btn btn-link join" data-room-id="<?php echo $room['id']; ?>">Joindre</button></td>
+											<td><?php echo $room['room_id']; ?></td>
+											<td><?php echo $room['room_name']; ?></td>
+											<td><?php //echo $room['numConnected']; ?></td>
+											<td>
+												<?php if($room['room_type'] == 'public' || in_array($room['room_id'], $allowedRooms)): ?>
+												<button class="btn btn-link join" data-room-id="<?php echo $room['room_id']; ?>" data-room-name="<?php echo $room['room_name']; ?>">Joindre</button>
+												<?php else: ?>
+												<button class="btn btn-link join disabled" data-room-id="<?php echo $room['room_id']; ?>" data-room-name="<?php echo $room['room_name']; ?>" disabled="disabled">Salon protégé</button>
+												<?php endif; ?>
+											</td>
+											<td>
+												<a href="#" class="tooltip-top" title="Opérateurs du salon"><span class="icon-briefcase"></span></a>
+											</td>
 										</tr>
 
 										<?php
@@ -189,8 +232,53 @@
 							?>
 						</tbody>	
 					</table>
+			</div> <!-- /#roomsList -->
+
+			<div id="dataRooms">
+				
 			</div>
 
+		</div>
+
+		<!-- Modals -->
+		<div class="modal hide fade" id="modalNewRoom">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h3>Créer un salon</h3>
+			</div>
+			<div class="modal-body">
+				<p>
+					Il existe plusieurs types de salons : 
+					<ul>
+						<li><strong>les salons publiques</strong>, qui sont accessibles à n'importe qui ; </li>
+						<li><strong>les salons protégés</strong>, nécessitant une invitation ; et</li>
+						<li><strong>les salons privés</strong>, qui nécessitent une invitation et ne sont pas affichés dans la liste des salons.</li>
+					</ul>
+
+					<form class="form-horizontal">
+						<div class="control-group">
+							<label class="control-label" for="newRoomName">Nom du salon</label>
+							<div class="controls">
+								<input type="text" id="newRoomName" placeholder="Espaces interdites">
+							</div>
+						</div>
+						<div class="control-group">
+							<label class="control-label" for="newRoomType">Type de salon</label>
+							<div class="controls">
+								<select id="newRoomType">
+									<option value="public">Salon public</option>
+									<option value="protected">Salon protégé</option>
+									<option value="private">Salon privé</option>
+								</select>
+							</div>
+						</div>
+					</form>
+				</p>
+			</div>
+			<div class="modal-footer">
+				<a href="#" class="btn" data-dismiss="modal">Annuler</a>
+				<a href="#" class="btn btn-primary" id="newRoomCreate" data-loading-text="Création...">Créer le salon</a>
+			</div>
 		</div>
 
 	</div>
@@ -199,24 +287,60 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="http://code.jquery.com/jquery-migrate-1.1.0.min.js"></script>
     <script src="misc/js/bootstrap.js"></script>
     <!--<script src="misc/js/chat.js"></script>-->
     <script type="text/javascript">
     	(function($) {
 
+    		// Utils
+    		var nl2br = function(str) {
+    			// Thx to http://phpjs.org/functions/nl2br/
+    			return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '<br ' + '/>' + '$2');
+    		};
+
 			$(document).ready(function() {
 
-				var lastMessageAuthor = '',
-					me = {
+				var me = {
 						id: '<?php echo $_SESSION['user']['id']; ?>',
 						name: '<?php echo $_SESSION['user']['name']; ?>'
 					},
-					$messageText = $('#messageText'),
-					$messages    = $('#content .wrapper'),
-					$content     = $('#content'),
-					$roomsList   = $('#roomsList'),
 
-					data;
+					$messageText   = $('#messageText'),
+					$messages      = $('#content .wrapper'),
+					$content       = $('#content'),
+					$roomsList     = $('#roomsList'),
+
+					authKey        = '<?php echo $token; ?>',
+
+					rooms          = new Array(),
+					avaliableRooms = new Array(),
+					allowedRooms   = new Array(),
+					typeaheadRooms = new Array(),
+					$data          = $('#dataRooms'),
+					currentRoom    = null;
+
+				// Generated list of avaliable (public and protected) rooms.
+				<?php
+					foreach($rooms AS $room) {
+						echo 'avaliableRooms[' . $room['room_id'] . '] = new Array(); avaliableRooms[' . $room['room_id'] . '][\'name\'] = \'' . $room['room_name'] . '\'; avaliableRooms[' . $room['room_id'] . '][\'type\'] = \'' . $room['room_type'] . '\';' . "\n";
+					}
+				?>
+				<?php
+					foreach($rooms AS $room) {
+						echo 'typeaheadRooms[' . ($room['room_id'] - 1) . '] = \'' . $room['room_name'] . '\'' . "\n";
+					}
+				?>
+				<?php
+					foreach($allowedRooms AS $roomId) {
+						echo 'allowedRooms[' . $roomId . '] = \'' . $roomId . '\'' . "\n";
+					}
+				?>
+
+				$.ajaxSetup({
+					url: 'do.php',
+					type: 'POST'
+				});
 
 				// Set the height of the chatbox
 				var updateContentHeight = function() {
@@ -227,8 +351,68 @@
 				$(window).resize(updateContentHeight);
 				updateContentHeight();
 
+				$('.tooltip-top').tooltip({placement: 'top'});
+
 				// Show list of rooms on startup.
 				$messages.html($roomsList.html());
+
+
+				// New rooms
+				$('.newRoom').live('click', function() {
+					$('#modalNewRoom').modal('show');
+				});
+				$('#newRoomCreate').click(function() {
+					var $button = $('#newRoomCreate');
+					$button.button('loading');
+
+					var $name = $('#newRoomName'),
+						$type = $('#newRoomType');
+
+					if($name.val() == '') {
+						$name.parent().parent().addClass('error');
+						$button.button('reset');
+					}
+					else {
+						$.ajax({
+							data: {
+								name: $name.val(),
+								type: $type.val(),
+								do: 'newRoom',
+								auth: authKey
+							}, 
+							success: function(id) {
+								if(id == 0) { // SQL error
+									var oldName = $button.val();
+									$button.button('reset').addClass('btn-error').val('Erreur');
+									setTimeout(function() {
+										$button.removeClass('btn-error').val(oldName);
+									}, 4000);
+									return;
+								}
+								$('#modalNewRoom').modal('hide');
+								var name = $name.val().replace(' ', '');
+								var row = '<tr><td>' + id + '</td><td>' + name + '</td><td>0</td><td><a href="#" class="join" data-room-id="' + id + ' data-room-name="' + name + '">Joindre</a></td><td></td></tr>';
+								if($('#tableRooms tbody tr:first').hasClass('noRooms')) {
+									$('#tableRooms tbody').html(row);
+								}
+								else {
+									$('#tableRooms tbody').append(row);
+								}
+
+								$name.val('');
+
+								document.reload;
+							},
+							error: function() {
+								var oldName = $button.val();
+								$button.button('reset').addClass('btn-error').val('Erreur');
+								setTimeout(function() {
+									$button.removeClass('btn-error').val(oldName);
+								}, 4000);
+							}
+						});
+					}
+				});
 
 
 
@@ -241,7 +425,8 @@
 						preciseDate = date + ':' + dateOb.getSeconds();
 					}
 					
-					if(lastMessageAuthor != author) {
+					message = nl2br(message);
+					if(rooms[currentRoom]['lastMessageAuthor'] != author) {
 						return '<p><strong>' + author + '</strong></p><div class="muted pull-right" title="' + preciseDate + '">' + date + '</div><p></p><p>' + message + '</p>';
 					}
 					else {
@@ -250,15 +435,151 @@
 
 				};
 
-				// Send a message
-				var sendMessage = function() {
+				var generateUnnamedHTMLMessage = function(message) {
+					message = nl2br(message);
+					return '<div class="well well-small">' + message + '</div>';
+				};
 
-					var htmlMessage = generateHTMLMessage($messageText.val(), me.name);
-					
-					$messages.append(htmlMessage);
+
+				// Duplicate the insertion of the message in the hidden data div.
+				var appendMessage = function(content, roomId) {
+					$messages.append(content);
 					$content.scrollTop(100000);
 
-					lastMessageAuthor = me.name;
+					$('#data-room-' + roomId).append(content);
+				};
+
+
+
+				
+				// Join & switch room
+				var $menuRoomsList = $('#menuRoomsList'),
+					$roomName  = $('#roomName'),
+					$listConnectedUsers = $('#listConnectedUsers'), 
+					$frameListConnectedUsers = $('#frameListConnectedUsers');
+				$('.secretRoom').typeahead({
+					source: typeaheadRooms,
+					items: 8
+				});
+				$('.formSecretRoom').live('submit', function() {
+					var roomName = $('.secretRoom').val();
+					$.ajax({
+						dataType: 'json',
+						data: {
+							auth: authKey,
+							do: 'dataFromRoomName',
+							roomName: roomName
+						},
+						success: function(room) {
+							if(room.error == 'unknow') {
+								$('.formSecretRoom').addClass('error');
+								return;
+							}
+							if(room.type == 'public' || allowedRooms[room.id] != undefined) {
+								$('.secretRoom').val('');
+								$('.formSecretRoom').removeClass('error');
+								joinRoom(room.id, roomName);
+							}
+						}
+					})
+				})
+
+				var switchRoom = function(roomId) {
+					$messageText.removeAttr('disabled');
+					$roomName.text(rooms[roomId]['name']);
+					$messages.html($('#data-room-' + roomId).html());
+					currentRoom = roomId;
+
+					$('#menuRoomsList li, .goToListRooms').removeClass('active');
+					$('#goToRoom' + roomId).addClass('active');
+
+					reloadUIConnectedUsers(rooms[roomId]['connected']);
+				};
+
+				var joinRoom = function(roomId, roomName) {
+					$('#listRoomsNoRooms').hide();
+					$('.goToListRooms').removeClass('active');
+					$('.noticeNotConnected').hide();
+
+					if(rooms[roomId] == undefined) {
+						var html = '<li class="active" data-id="' + roomId + '" id="goToRoom' + roomId + '"><a href="#">' + roomName + '</a></li>';
+						$menuRoomsList.append(html);
+
+						$messageText.removeAttr('disabled');
+						$roomName.text(roomName);
+
+						currentRoom = roomId;
+
+						rooms[currentRoom] = new Array();
+						rooms[currentRoom]['name'] = roomName;
+						rooms[currentRoom]['lastMessageAuthor'] = null;
+
+						$data.append('<div id="data-room-' + currentRoom + '"></div>');
+
+						$messages.html('');
+						appendMessage(generateUnnamedHTMLMessage('Vous avez rejoint le salon.'), currentRoom);
+
+						// Under the hole
+						$.ajax({
+							data: {
+								do: 'join',
+								auth: authKey,
+								room: roomId
+							},
+							dataType: 'json',
+							success: function(connected) {
+								rooms[currentRoom]['connected'] = connected;
+								reloadUIConnectedUsers(connected);
+							}
+						});
+					}
+					else {
+						switchRoom(roomId);
+					}
+				};
+
+				var reloadUIConnectedUsers = function(connected) {
+					$listConnectedUsers.html('');
+					$frameListConnectedUsers.show();
+					$listConnectedUsers.append('<li class="nav-header">Utilisateurs connectés</li>');
+					for(userRow in connected) {
+						$listConnectedUsers.append('<li><a href="#" data-user-id="' + connected[userRow].userId + '">' + connected[userRow].name + '</a></li>');
+					}
+				};
+
+				$('#menuRoomsList li').live('click', function() {
+					switchRoom($(this).data('id'));
+				});
+
+				$('.join').live('click', function(event) {
+					joinRoom($(this).data('room-id'), $(this).data('room-name'));
+				});
+				
+				$('.goToListRooms').click(function() {
+					currentRoom = null;
+					$messageText.attr('disabled', 'disabled');
+					$messages.html($roomsList.html());
+					$('#menuRoomsList li').removeClass('active');
+					$(this).addClass('active');
+					$roomName.text('Tous les salons');
+					$frameListConnectedUsers.hide();
+				});
+
+
+				// Send a message
+				var sendMessage = function() {
+					var regexMe = /^\/me/i;
+					var htmlMessage;
+					if(regexMe.test($messageText.val())) {
+						htmlMessage = generateUnnamedHTMLMessage('<strong>' + me.name + '</strong>' + $messageText.val().replace('/me ', ' '));
+						rooms[currentRoom]['lastMessageAuthor'] = null;
+					}
+					else {
+						htmlMessage = generateHTMLMessage($messageText.val(), me.name);
+						rooms[currentRoom]['lastMessageAuthor'] = me.name;
+					}
+					
+					appendMessage(htmlMessage, currentRoom);
 
 					$messageText.val('').focus();
 				};
---
+++ do.php
@@ -0,0 +1,77 @@
+<?php
+	require_once('connect.php');
+	require_once('salt.php');
+	session_start();
+
+	$pdo = SQLiteConnect();
+
+	if(!isset($_POST['auth'])) {
+		exit;
+	}
+	else if(sha1(SALT . $_POST['auth']) != $_SESSION['auth']) {
+		exit;
+	}
+
+	switch($_POST['do']) {
+		case 'newRoom':
+			$_POST['name'] = str_replace(' ', '', $_POST['name']);
+			$_POST['name'] = str_replace('\'', '', $_POST['name']);
+
+			$exists = $pdo->prepare('SELECT id FROM rooms WHERE name = ?');
+			$exists->execute(array($_POST['name']));
+			if($exists->fetchAll() != array()) {
+				echo 'exists'; exit;
+			}
+
+			$query = $pdo->prepare('INSERT INTO rooms (name, type, creator, added) VALUES (?, ?, ?, date(\'now\'))');
+			
+			if($query->execute(array($_POST['name'], $_POST['type'], $_SESSION['user']['id']))) {
+				$id = $pdo->query('SELECT id FROM rooms ORDER BY id DESC LIMIT 1')->fetchAll();
+
+				// The creator must be allowed to access his room.
+				if($_POST['type'] != 'public') {
+					$query = $pdo->prepare('INSERT INTO usersAllowed (roomId, userId) VALUES (?, ?)');
+					$query->execute(array($id[0]['id'], $_SESSION['user']['id']));
+				}
+				echo $id[0]['id'];
+			}
+			else {
+				echo 0;
+			}
+			break;
+
+		case 'join':
+			$query = $pdo->prepare('SELECT userId FROM usersRooms WHERE roomId = ? AND userId = ?');
+			$query->execute(array($_POST['room'], $_SESSION['user']['id']));
+
+			if($query->fetchAll() == array()) {
+				$query = $pdo->prepare('INSERT INTO usersRooms(roomId, userId) VALUES (?, ?)');
+				$query->execute(array($_POST['room'], $_SESSION['user']['id']));
+			}
+
+			$query = $pdo->prepare('SELECT usersRooms.userId,
+										   users.name
+									FROM usersRooms
+									INNER JOIN users
+										ON usersRooms.userId = users.id
+									WHERE usersRooms.roomId = ?');
+			$query->execute(array($_POST['room']));
+
+			echo json_encode($query->fetchAll());
+			break;
+
+		case 'dataFromRoomName':
+			$ids = $pdo->prepare('SELECT id, type FROM rooms WHERE name = ?');
+			$ids->execute(array($_POST['roomName']));
+			$ids = $ids->fetchAll();
+			$response;
+			if($ids == array()){
+				$response = array('error' => 'unknow');
+			}
+			else {
+				$response = array('error' => 'done', 
+								  'id'    => $ids[0]['id'], 
+								  'type'  => $ids[0]['type']);
+			}
+			echo json_encode($response);
+	}
